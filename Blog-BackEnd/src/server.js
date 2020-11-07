1359934db7e7c73dfbeb8b763717968bb4c680ba
Jesus Esquer <jme2791@gmail.com>
2020-11-06 20:46:57 -0700

    created database and articles table

--- Blog-BackEnd/src/server.js
+++ Blog-BackEnd/src/server.js
@@ -3,21 +3,7 @@ import bodyParser from "body-parser";
 
 const app = express();
 
-// Sample articles
-const articlesInfo = {
-  "learn-react": {
-    upvotes: 0,
-    comments: [],
-  },
-  "learn-node": {
-    upvotes: 0,
-    comments: [],
-  },
-  "my-thoughts-on-resumes": {
-    upvotes: 0,
-    comments: [],
-  },
-};
+
 
 app.use(bodyParser.json());
 
@@ -31,6 +17,12 @@ app.post("/api/articles/:name/upvote", (req, res) => {
     );
 });
 
-app.post("/api/articles/:name/add-comment", (req, res) => {});
+app.post("/api/articles/:name/add-comment", (req, res) => {
+  const { username, text } = req.body;
+  const articleName = req.params.name;
+
+  articlesInfo[articleName].comments.push({ username, text });
+  res.status(200).send(articlesInfo[articleName]);
+});
 
 app.listen(8000, () => console.log("Listening on port 8000"));
