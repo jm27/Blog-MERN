3455086bfe0fd8594a8fe693233b8b3da6fd6414
Jesus Esquer <jme2791@gmail.com>
2020-11-06 19:40:07 -0700

    upvote and comment route

--- Blog-BackEnd/src/server.js
+++ Blog-BackEnd/src/server.js
@@ -1,7 +1,36 @@
 import express from "express";
+import bodyParser from "body-parser";
 
 const app = express();
 
-app.get("/hello", (req, res) => res.send("Hello!"));
+// Sample articles
+const articlesInfo = {
+  "learn-react": {
+    upvotes: 0,
+    comments: [],
+  },
+  "learn-node": {
+    upvotes: 0,
+    comments: [],
+  },
+  "my-thoughts-on-resumes": {
+    upvotes: 0,
+    comments: [],
+  },
+};
+
+app.use(bodyParser.json());
+
+app.post("/api/articles/:name/upvote", (req, res) => {
+  const articleName = req.params.name;
+  articlesInfo[articleName].upvotes += 1;
+  res
+    .status(200)
+    .send(
+      `${articleName} now has ${articlesInfo[articleName].upvotes} upvotes!`
+    );
+});
+
+app.post("/api/articles/:name/add-comment", (req, res) => {});
 
 app.listen(8000, () => console.log("Listening on port 8000"));
